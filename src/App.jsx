8d6478eff86c9bab1e974ed7bd1ffb5787c1775e
Angelica Garcia <anyegar36@gmail.com>
2023-06-03 09:51:32 -0500

    Refactor 'app' file into a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom'
+import { Welcome } from './components/Welcome'
 import './App.css'
 
 function App () {
@@ -8,18 +10,20 @@ function App () {
         alt='background'
         className='w-full sticky top-0 h-[18vh] object-cover'
       />
-      <section className='flex flex-col justify-center items-center m-4'>
-        <div className='flex flex-col items-center justify-between border-primary border p-5 rounded-3xl max-w-[600px] w-full h-[75vh] shadow-2xl'>
-          <div className='flex gap-4 items-center justify-center text-secondary text-2xl text-center'>
-            <span className='icon-book text-6xl' />
-            <h1 className='font-bold'>Cuaderno <br /> Digital</h1>
-          </div>
-          <h2 className='text-xl font-bold'>Bienvenido a tu cuaderno digital</h2>
-          <p className='text-center'>Lleva tu cuaderno en la palma de tu mano. Agrega, organiza y accede a tus notas de forma fácil y rápida con nuestra página web de Cuaderno Digital. Toma el control de tus ideas estés donde estés.</p>
-          <button>Registrarse</button>
-          <button>Iniciar sesión</button>
-        </div>
-      </section>
+      <Routes>
+        <Route
+          path='/'
+          element={<Welcome />}
+        />
+        <Route
+          path='/register'
+          element={<Welcome />}
+        />
+        <Route
+          path='/login'
+          element={<Welcome />}
+        />
+      </Routes>
       <footer className='w-full bg-primary sticky bottom-0 h-[30px]' />
     </>
   )
