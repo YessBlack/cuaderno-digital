34d6168e94a3e4f2565eb30902c85158eb344e9e
Angelica Garcia <anyegar36@gmail.com>
2023-06-03 18:12:26 -0500

    add feature: registration with email and password

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,9 +6,8 @@ const auth = getAuth()
 export const createUser = async (email, password) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-    console.log(userCredential)
     return userCredential
   } catch (error) {
-    return error
+    return error.message
   }
 }
