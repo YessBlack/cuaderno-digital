ff7591ae3c8abbc36c298ed795208d57046a303b
Angelica Garcia <anyegar36@gmail.com>
2025-09-29 20:50:20 -0500

    add formatDate util

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -21,3 +21,11 @@ export const getDateTime = () => {
   const dateNow = new Date()
   return dateNow.toISOString()
 }
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+  return `${day < 10 ? `0${day}` : day}/${month < 10 ? `0${month}` : month}/${year}`
+}
