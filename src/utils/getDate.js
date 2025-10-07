886fa7f952d8229fe27c450ecc6eb08ecb2672a1
Angelica Garcia <anyegar36@gmail.com>
2025-10-07 07:44:59 -0500

    update date utils

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -38,3 +38,21 @@ export const getMinutes = () => {
   const dateNow = new Date()
   return dateNow.getMinutes()
 }
+
+export const formatDate = (date) => {
+  const d = new Date(date)
+  const day = (`0${d.getDate()}`).slice(-2)
+  const month = (`0${d.getMonth() + 1}`).slice(-2)
+  const year = d.getFullYear()
+  return `${year}-${month}-${day}`
+}
+
+export const formatDateTime = (date) => {
+  const d = new Date(date)
+  const day = (`0${d.getDate()}`).slice(-2)
+  const month = (`0${d.getMonth() + 1}`).slice(-2)
+  const year = d.getFullYear()
+  const hours = (`0${d.getHours()}`).slice(-2)
+  const minutes = (`0${d.getMinutes()}`).slice(-2)
+  return `${year}-${month}-${day} ${hours}:${minutes}`
+}
