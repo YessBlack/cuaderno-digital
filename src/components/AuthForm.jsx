4407e3df21a1f642480fd9afe41bc585c3e27e4b
Angelica Garcia <anyegar36@gmail.com>
2025-11-07 13:53:18 -0500

    fix finales

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,7 +1,9 @@
 import { useTranslation } from 'react-i18next'
 
-export function AuthForm ({ title, buttonText, handleSubmit, showNameField }) {
+export function AuthForm({ title, buttonText, handleSubmit, showNameField }) {
   const { t } = useTranslation()
+
+  console.log('Formulario de Autenticacion')
 
   return (
     <section className='flex flex-col justify-center items-center m-4'>
@@ -10,11 +12,11 @@ export function AuthForm ({ title, buttonText, handleSubmit, showNameField }) {
         <form className='flex flex-col gap-4 w-[250px]' onSubmit={handleSubmit}>
           {
             showNameField &&
-              <input
-                type='text'
-                className='border border-secondary p-2 rounded-xl focus:outline-none placeholder:text-center' placeholder={t('Nombre')}
-                name='name'
-              />
+            <input
+              type='text'
+              className='border border-secondary p-2 rounded-xl focus:outline-none placeholder:text-center' placeholder={t('Nombre')}
+              name='name'
+            />
           }
           <input
             type='email'
